578ba550985e6d0bb4247255c2b6b6c335225fc7
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-18 15:16:48 +0100

    Update space-import-map.ts

--- space-import-map.ts
+++ space-import-map.ts
@@ -1,10 +1,10 @@
 export const SPACE_IMPORT_MAP = {
   x: {
     spacePath: "root/x",
-    targetId: "target-space-id-here"
+    targetId: "x-01K0EVTW7HW9M520EMKD7K93HQ"
   },
   y: {
     spacePath: "root/y",
-    targetId: "another-target-id"
+    targetId: "x-01K0EVTW7HW9M520EMKD7K93HQ"
   }
 };
