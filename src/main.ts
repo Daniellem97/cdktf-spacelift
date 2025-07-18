e91111cb097d23d4e99cdb73d0876b7a487a77e5
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-18 18:07:35 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,17 @@ import { Space } from "../.gen/providers/spacelift/space/index.js";
 import { DataSpaceliftSpaceByPath } from "../.gen/providers/spacelift/data-spacelift-space-by-path/index.js";
 import { SpaceliftProvider } from "../.gen/providers/spacelift/provider/index.js";
 
-class Infra extends TerraformStack {
+class ImportStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     new SpaceliftProvider(this, "spacelift", {});
 
-    const root = new DataSpaceliftSpaceByPath(this, "root", {
-      spacePath: "root",
-    });
-
-    const imported = new Space(this, "x", {
-      name: "x",
-      parentSpaceId: root.id,
-      inheritEntities: true,
-    });
-
-    // ✅ THIS is what marks the resource as "imported"
-    imported.overrideLogicalId("x");
-    imported.addOverride("import", "x-01K0EVTW7HW9M520EMKD7K93HQ");
+    // This tells CDKTF to emit import instructions for Terraform
+    Space.generateConfigForImport(this, "x", "x-01K0EVTW7HW9M520EMKD7K93HQ");
   }
 }
 
 const app = new App();
-new Infra(app, "infra");
+new ImportStack(app, "import-x");
 app.synth();
