64de7cc55b54dd70a5d690c464ed01540da3aa49
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-18 15:13:18 +0100

    Create space-import-map.ts

---
+++ space-import-map.ts
@@ -0,0 +1,10 @@
+export const SPACE_IMPORT_MAP = {
+  x: {
+    spacePath: "root/x",
+    targetId: "target-space-id-here"
+  },
+  y: {
+    spacePath: "root/y",
+    targetId: "another-target-id"
+  }
+};
