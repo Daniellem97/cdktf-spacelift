0bcd0244ecd640bd38bb1db1439ade270141da50
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-21 12:23:40 +0100

    Update space-import-map.ts

--- src/space-import-map.ts
+++ src/space-import-map.ts
@@ -1,6 +1,6 @@
 export const SPACE_IMPORT_MAP = {
   x: {
     spacePath: "root/x",
-    targetId: "x-01K0EVTW7HW9M520EMKD7K93HQ", // from UI
+    targetId: "x-01K0EVTW7HW9M520EMKD7K93HQ",
   },
 };
