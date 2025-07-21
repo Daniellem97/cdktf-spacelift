e8fd1fef53724bef22aa3fb330c0682bba79b3b3
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-07-21 12:24:53 +0100

    Update management-misc-stack.ts

--- src/stacks/management-misc-stack.ts
+++ src/stacks/management-misc-stack.ts
@@ -1,13 +1,11 @@
 import { Construct } from "constructs";
 import { TerraformStack } from "cdktf";
 import { Space } from "../../.gen/providers/spacelift/space/index.js";
-import { DataSpaceliftSpaceByPath } from "../../.gen/providers/spacelift/data-spacelift-space-by-path/index.js";
 
 export class ManagementMiscStack extends TerraformStack {
-  constructor(scope: Construct, id: string, space: DataSpaceliftSpaceByPath) {
+  constructor(scope: Construct, id: string, space: Space) {
     super(scope, id);
 
-    // Add example resources here
-    // You can use the space.id or space.path to configure resources scoped to the space
+    // Use space.id, space.name, etc.
   }
 }
